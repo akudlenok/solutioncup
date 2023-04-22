470b5ff472d3d76ff4d05f0f8d7aed949a15e6d9
akudlenok <akudlenok@yandex.ru>
2023-04-22 15:17:59 +0300

    month

---
+++ src/constants/month.ts
@@ -0,0 +1,42 @@
+export const JANUARY = 1;
+export const FEBRUARY = 2;
+export const MARCH = 3;
+export const APRIL = 4;
+export const MAY = 5;
+export const JUNE = 6;
+export const JULY = 7;
+export const AUGUST = 8;
+export const SEPTEMBER = 9;
+export const OCTOBER = 10;
+export const NOVEMBER = 11;
+export const DECEMBER = 12;
+
+export const MONTH_IDS = [
+  JANUARY,
+  FEBRUARY,
+  MARCH,
+  APRIL,
+  MAY,
+  JUNE,
+  JULY,
+  AUGUST,
+  SEPTEMBER,
+  OCTOBER,
+  NOVEMBER,
+  DECEMBER,
+];
+
+export const MONTHS: any = {
+  [JANUARY]: 'Январь',
+  [FEBRUARY]: 'Февраль',
+  [MARCH]: 'Март',
+  [APRIL]: 'Апрель',
+  [MAY]: 'Май',
+  [JUNE]: 'Июнь',
+  [JULY]: 'Июль',
+  [AUGUST]: 'Август',
+  [SEPTEMBER]: 'Сентябрь',
+  [OCTOBER]: 'Октябрь',
+  [NOVEMBER]: 'Ноябрь',
+  [DECEMBER]: 'Декабрь',
+};
--- src/pages/ExpensesPage/ExpensesPage.tsx
+++ src/pages/ExpensesPage/ExpensesPage.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import PageTitle from 'components/PageTitle/PageTitle';
+import { MONTHS } from 'constants/month';
 
 const ExpensesPage: FC = (): JSX.Element => {
+  const pageTitle = `Расходы за ${MONTHS[new Date().getMonth() + 1]}`;
   return (
-    <PageTitle title='Расходы'/>
+    <PageTitle title={pageTitle} />
   );
-}
+};
 
 
 export default ExpensesPage;
